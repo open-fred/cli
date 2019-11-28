aa350d350425fe01f6a76ed843bc21735fcd6482
Stephan Günther <gnn.code@gmail.com>
2019-11-28 20:04:44 +0100

    Blacken `setup.py`

--- setup.py
+++ setup.py
@@ -18,62 +18,69 @@ from setuptools import setup
 def read(*names, **kwargs):
     with io.open(
         join(dirname(__file__), *names),
-        encoding=kwargs.get('encoding', 'utf8')
+        encoding=kwargs.get("encoding", "utf8"),
     ) as fh:
         return fh.read()
 
 
 setup(
-    name='open_FRED-cli',
-    version='0.0.0dev',
-    license='BSD-3-Clause',
-    description='"The open_FRED command line interface and other tools for working with open_FRED data."',
-    long_description='%s\n%s' % (
-        re.compile('^.. start-badges.*^.. end-badges', re.M | re.S).sub('', read('README.rst')),
-        re.sub(':[a-z]+:`~?(.*?)`', r'``\1``', read('CHANGELOG.rst'))
+    name="open_FRED-cli",
+    version="0.0.0dev",
+    license="BSD-3-Clause",
+    description=(
+        "The open_FRED command line interface and other tools "
+        "for working with open_FRED data."
     ),
-    author='Stephan Günther',
-    author_email='gnn.code@gmail.com',
-    url='https://github.com/open-fred/cli',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
-    py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
+    long_description="%s\n%s"
+    % (
+        re.compile("^.. start-badges.*^.. end-badges", re.M | re.S).sub(
+            "", read("README.rst")
+        ),
+        re.sub(":[a-z]+:`~?(.*?)`", r"``\1``", read("CHANGELOG.rst")),
+    ),
+    author="Stephan Günther",
+    author_email="gnn.code@gmail.com",
+    url="https://github.com/open-fred/cli",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    py_modules=[splitext(basename(path))[0] for path in glob("src/*.py")],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: Unix',
-        'Operating System :: POSIX',
-        'Operating System :: Microsoft :: Windows',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
+        # complete classifier list:
+        # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: Unix",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Programming Language :: Python :: Implementation :: PyPy",
         # uncomment if you test on these interpreters:
         # 'Programming Language :: Python :: Implementation :: IronPython',
         # 'Programming Language :: Python :: Implementation :: Jython',
         # 'Programming Language :: Python :: Implementation :: Stackless',
-        'Topic :: Utilities',
+        "Topic :: Utilities",
     ],
     project_urls={
-        'Documentation': 'https://cli.readthedocs.io/',
-        'Changelog': 'https://cli.readthedocs.io/en/latest/changelog.html',
-        'Issue Tracker': 'https://github.com/open-fred/cli/issues',
+        "Documentation": "https://cli.readthedocs.io/",
+        "Changelog": "https://cli.readthedocs.io/en/latest/changelog.html",
+        "Issue Tracker": "https://github.com/open-fred/cli/issues",
     },
     keywords=[
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     install_requires=[
-        'click',
+        "click",
         # eg: 'aspectlib==1.1.1', 'six>=1.7',
     ],
     extras_require={
@@ -81,9 +88,5 @@ setup(
         #   'rst': ['docutils>=0.11'],
         #   ':python_version=="2.6"': ['argparse'],
     },
-    entry_points={
-        'console_scripts': [
-            'open_FRED = open_FRED.cli:main',
-        ]
-    },
+    entry_points={"console_scripts": ["open_FRED = open_FRED.cli:main"]},
 )
